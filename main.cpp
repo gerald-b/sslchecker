fb0b40fa830ab5610ca5d25262b9578e118433b3
Gerald Büchler <dev@genomast.de>
2020-06-29 22:50:04 +0200

    added some errorhandling for language loading

--- main.cpp
+++ main.cpp
@@ -3,16 +3,46 @@
 #include "mysettings.hpp"
 #include <QApplication>
 #include <QTranslator>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
-
     QString langFile = QString("./language/").append(MySettings::getInstance()->value("language/current").toString()).append(".qm");
-    QTranslator *lang = new QTranslator(&a);
-    lang->load(langFile);
-    a.installTranslator(lang);
-
+    QString errMsg = "";
+    bool hasLanguage = true;
+    if (!QFile::exists(langFile))
+    {
+        errMsg += QObject::tr("FILE");
+        errMsg += " " + langFile + " ";
+        errMsg += QObject::tr("NOT FOUND");
+        errMsg += "\n\n";
+        errMsg += QObject::tr("TRY TO LOAD FALLBACK");
+        QMessageBox::warning(nullptr,QObject::tr("WARNING"),errMsg,QMessageBox::Ok,QMessageBox::Ok);
+        langFile = QString("./language/english.qm");
+        if (!QFile::exists(langFile))
+        {
+            errMsg = QObject::tr("FALLBACK LANGUAGE (ENGLISH) ALSO NOT FOUND");
+            errMsg += "\n\n";
+            errMsg += QObject::tr("RUNNING ON DEVELOPMENT NAMING!");
+            QMessageBox::warning(nullptr,QObject::tr("WARNING"),errMsg,QMessageBox::Ok,QMessageBox::Ok);
+            hasLanguage = false;
+        }
+        else
+        {
+            errMsg = QObject::tr("FALLBACK LANGUAGE (ENGLISH) FOUND");
+            errMsg += "\n\n";
+            errMsg += QObject::tr("PROCEED WITH THIS");
+            QMessageBox::information(nullptr,QObject::tr("INFORMATION"),errMsg,QMessageBox::Ok,QMessageBox::Ok);
+        }
+    }
+    if (hasLanguage)
+    {
+        QTranslator *lang = new QTranslator(&a);
+        lang->load(langFile);
+        a.installTranslator(lang);
+    }
     FrmMain w;
     w.show();
     return a.exec();

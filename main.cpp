ca8e76ec66fbf9add52a86a8adfd311218bfaf06
Gerald Büchler <dev@genomast.de>
2020-06-29 22:46:03 +0200

    prepare the project for multilanguage and load the language from settings

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "frmmain.hpp"
 
+#include "mysettings.hpp"
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString langFile = QString("./language/").append(MySettings::getInstance()->value("language/current").toString()).append(".qm");
+    QTranslator *lang = new QTranslator(&a);
+    lang->load(langFile);
+    a.installTranslator(lang);
+
     FrmMain w;
     w.show();
     return a.exec();

d925708f5debaf6b1ef99ff166fa59176a344915
Gerald Büchler <dev@genomast.de>
2020-07-04 12:50:54 +0200

    Created empty english.ts file (lupdate)

---
+++ english.ts
@@ -0,0 +1,189 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FrmDetail</name>
+    <message>
+        <source>FRMDETAIL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CERTID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CERTNAME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CERTSTATUS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>VALIDDATES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DATEFROM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DATETO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ADD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>REMOVE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DOMAINS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DOMAINNAME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HOSTS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HOSTNAME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALERTINTERVAL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALERTLEVEL_1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALERTLEVEL_2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALERTLEVEL_3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALERTLEVEL_4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALERTLEVEL_5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ALERTMAIL (ONEPERLINE)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;FILE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;EXIT</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FrmMain</name>
+    <message>
+        <source>FRMMAIN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>NAME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DATEFROM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DATETO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AFFECTEDDOMAINS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>AFFECTEDHOSTS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ADD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>REMOVE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DETAILS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;FILE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;EXIT</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>FILE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>NOT FOUND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TRY TO LOAD FALLBACK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>WARNING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>FALLBACK LANGUAGE (ENGLISH) ALSO NOT FOUND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>RUNNING ON DEVELOPMENT NAMING!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>FALLBACK LANGUAGE (ENGLISH) FOUND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PROCEED WITH THIS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>INFORMATION</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- sslchecker.pro
+++ sslchecker.pro
@@ -45,4 +45,5 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 DISTFILES += \
+    english.ts \
     german.ts
